6ac825d6fa2a054122837145996c1716019b21ac
Kyle Thompson <kt@deeper.io>
2025-01-19 23:56:01 -0600

    init commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using RateLims;
+
+var limiter = new SlidingWindowRateLimitService();
+var key = "foo";
+
+foreach (var i in Enumerable.Range(0, 100))
+{
+    using var lease = limiter.Acquire(key);
+    if (!lease.IsAcquired)
+    {
+        Console.WriteLine($"Rate Limited {i}");
+        await Task.Delay(1000);
+        continue;
+    }
+
+    Console.WriteLine($"{i}");
+}
